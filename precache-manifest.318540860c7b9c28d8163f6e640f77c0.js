a5f3b359f2680c4155658adec9b3528aa639a16d
Vineethcosta <reddyvineeth6@gmail.com>
2018-11-26 18:06:26 +0530

    Updates

---
+++ precache-manifest.318540860c7b9c28d8163f6e640f77c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cffa739f751f90686ec3ac6e9b56e0d",
+    "url": "/frontend/static/media/icon-check.4cffa739.svg"
+  },
+  {
+    "revision": "5cbbaf94823758d3c60e",
+    "url": "/frontend/static/js/runtime~main.5cbbaf94.js"
+  },
+  {
+    "revision": "71aedf1ee85c63791bcc",
+    "url": "/frontend/static/js/main.71aedf1e.chunk.js"
+  },
+  {
+    "revision": "ab4e70560cb6cc6832ec",
+    "url": "/frontend/static/js/1.ab4e7056.chunk.js"
+  },
+  {
+    "revision": "71aedf1ee85c63791bcc",
+    "url": "/frontend/static/css/main.cd9176e3.chunk.css"
+  },
+  {
+    "revision": "e90408d9da4bfde7ce7e474aa44588d6",
+    "url": "/frontend/index.html"
+  }
+];
